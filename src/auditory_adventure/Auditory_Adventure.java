0f7c8473d5270c60d6f8abd8acb5f8d8a8304e83
dam2 <dam2@a223e22.dc.domenicoscarlatti.es>
2024-10-22 17:00:19 +0200

    iniciando

---
+++ src/auditory_adventure/Auditory_Adventure.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package auditory_adventure;
+
+/**
+ *
+ * @author dam2
+ */
+public class Auditory_Adventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenidass!");
+    }
+    
+}
