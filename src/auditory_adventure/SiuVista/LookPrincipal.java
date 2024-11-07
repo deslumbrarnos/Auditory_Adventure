d39db8271c8969791b117de1e2395a53240d2cd1
dam2 <dam2@a223e22.dc.domenicoscarlatti.es>
2024-11-07 18:01:40 +0100

    cambio

--- src/auditory_adventure/SiuVista/LookPrincipal.java
+++ src/auditory_adventure/SiuVista/LookPrincipal.java
@@ -14,6 +14,7 @@ public class LookPrincipal extends javax.swing.JFrame {
      * Creates new form LookPrincipal
      */
     public LookPrincipal() {
+        System.out.println("hola");
         initComponents();
     }
 
